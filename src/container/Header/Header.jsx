3648baa230f07d9f2906f28f504a9ae7394d6df8
rumica <rumeysavar2@gmail.com>
2022-07-04 19:34:06 +0300

    Created SpecialMenu section and added styles.

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className="app__header app_wrapper section__padding" id="home">
+    <div className="app__header app__wrapper section__padding" id="home">
       <div className="app__wrapper_info">
         <SubHeading title="Chase the new flavour"/>
         <h1 className="app__header-h1">The Key To Fine Dining</h1>
